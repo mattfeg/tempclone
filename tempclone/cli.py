b961824caad2f35c339228b2dbd298290b72954f
Matheus Ferreira <ferreira@MacBook-Air-de-Matheus-2.local>
2024-05-20 11:02:15 -0300

    Remove debug messages of Fecthing API

--- tempclone/cli.py
+++ tempclone/cli.py
@@ -75,7 +75,6 @@ def list_templates():
     page = 1
     while True:
         url = f"{GITHUB_API_URL}/users/{owner}/repos?page={page}&per_page=100"
-        click.echo(f"Fetching URL: {url}")  # Debug output
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
             click.echo(f"Error listing templates: {response.json()}")
@@ -113,7 +112,6 @@ def new_project(new_repo_name):
     page = 1
     while True:
         url = f"{GITHUB_API_URL}/users/{owner}/repos?page={page}&per_page=100"
-        click.echo(f"Fetching URL: {url}")  # Debug output
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
             click.echo(f"Error listing templates: {response.json()}")
@@ -149,7 +147,7 @@ def new_project(new_repo_name):
 
         # Pause and check if the repository is ready
         click.echo('Waiting for the repository to be configured...')
-        time.sleep(5)  # Pause for 5 seconds
+        time.sleep(5)
 
         for _ in range(5):  # Try 5 times
             click.echo(f'Checking repository status: {new_repo_clone_url}')  # Debug output
